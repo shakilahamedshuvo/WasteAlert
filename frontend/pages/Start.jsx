b0a361ec5bf92ecee96e60d7d47899fdb30f39a8
shakilahamedshuvo <shuvo@southern.ac.bd>
2025-06-27 21:32:34 +0600

    ui change

--- frontend/pages/Start.jsx
+++ frontend/pages/Start.jsx
@@ -4,46 +4,31 @@ import {Button} from '@mui/material'
 import { Link } from 'react-router-dom';
 import { FaArrowCircleRight } from "react-icons/fa";
 import Wave from 'react-wavify'
+import start_bg from '../src/assets/image/start_bg.png'
 const Start = () => {
   return (
-    <div className='flex flex-col h-screen items-center justify-between '>
-      <p className='text-3xl font-bold text-[#528e4b] mt-10 font-[Nunito] letter-spacing-6'>
-         Welcome to WasteAlert
-      </p>
-      <img
-        src={startpage_img}
-        className="w-80 h-64  mb-8"
-        alt="Start Page"
-      />
-      <p className='text-2xl text-center font-semibold mx-1 text-[] font-[Roboto Condensed] lg:max-w-1/2 md:p-4' >
-        Make your neighborhood cleaner by reporting waste problems. We handle your complaints directly or coordinate with the waste management team to ensure they’re resolved.
-      </p>
-      <div className='flex flex-col gap-4 justify-between items-center w-full px-10 mt-10 text-white'>
-        <Link
-          to='/signup'
-          className='bg-[black] px-16 py-4 rounded-xl w-full text-center text-xl  lg:max-w-5xl max-w-2xl text-[Inter] font-bold'
+    <div className='bg-[#141F17] w-full h-screen flex flex-col justify-between'>
+        <div className='w-full h-[40%]'
+          style={{
+            backgroundImage: `url(${start_bg})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
         >
-          Sign Up   
-        </Link>
-        <Link
-          to='/login'
-          className='border  border-[black] px-16 py-4 rounded-xl w-full text-[black] text-center text-xl font-[Inter] lg:max-w-5xl max-w-2xl font-bold'
-        >
-          Login
-        </Link>
-      </div>
-      <Wave fill='#528e4b'
-        paused={false}
-        style={{ display: 'flex' }}
-        options={{
-          height: 50,
-          amplitude: 20,
-          speed: 0.15,
-          points: 3
-        }}
-      />
+        </div>
+        <div className=''>
+            <h1 className='mt-4 text-white font-["Spline Sans"] font-bold text-center text-[28px]'>WasteAlert</h1>
+            <p className='mt-2 text-white font-["Spline Sans"] text-center text-[20px] px-1'>Report issues, track collections, and stay informed about waste management in your community.</p>
+         
+        </div>
+         <div className=' flex flex-col mt-6 items-center justify-center gap-3 mb-6'>
+              <Link to='/signup' className='text-[#141F17] font-bold bg-green-300 px-auto w-4/5 py-4 font-[16px] rounded-[24px] text-center'>Sign Up</Link>
+               <Link to='/signup' className='text-white font-bold bg-[#294033] px-auto w-4/5 py-4 font-[16px] rounded-[24px] text-center'>Login</Link>
+
+           </div>
     </div>
   );
 };
 
 export default Start;
+
